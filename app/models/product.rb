3c8ff995accc17da3417dd8adf208dffe64dee1f
yuma0222 <nakamura_poison@icloud.com>
2020-07-18 18:29:46 +0900

    update model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
 class Product < ApplicationRecord
+	belongs_to :genre
+
+	has_many :order_products, dependent: :destroy
+	has_many :carts, dependent: :destroy
+
+    validates :genre_id, presence: true
+	validates :name, presence: true, uniqueness: true
+	validates :caption, presence: true
+	validates :image_id, presence: true
+	validates :price, numericality: true
+	validates :sale_status, inclusion: { in: [true, false] }
 end
