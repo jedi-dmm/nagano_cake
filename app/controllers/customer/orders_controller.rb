756f897fb7056e55d1ecf5be931f1ce26d80413b
vagrant <vagrant@localhost.localdomain>
2020-07-20 04:31:25 +0000

    [update]view file and controller about order

--- app/controllers/customer/orders_controller.rb
+++ app/controllers/customer/orders_controller.rb
@@ -5,16 +5,22 @@ class Customer::OrdersController < ApplicationController
 
 	def new
 		@order = Order.new
-		@order_product = Order_product.new
-		@cart = Cart.where(customer_id: current_customer.id)
-		@order.customer_id = current_customer.id
-		@order_product.customer_id = current_customer.id
+		# @order_product = Order_product.new
+		# @cart = Cart.where(customer_id: current_customer.id)
+		# @order.customer_id = current_customer.id
+		# @order_product.customer_id = current_customer.id
 	end
 
 	def create
 	end
 
 	def show
+		order = Order.find(params[:id])
+		order.order_products.each do |order_product|
+			subtotal = order_product.unit_price * order_product.product_quantity
+			total_fee = total_fee + subtotal
+		end
+		charge = order.postage + total_fee
 	end
 
 	def confirm
